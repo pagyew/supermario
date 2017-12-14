5faa09ad38c2a708cbaca1c5139fae486b2bfa61
6ecuw <fickleprogger@gmail.com>
2017-12-14 22:37:01 +0300

    Episode 4 complite

---
+++ public/js/Entity.js
@@ -0,0 +1,31 @@
+import {Vec2} from './math.js';
+
+export class Trait {
+  constructor(name) {
+    this.NAME = name;
+  }
+
+  update() {
+    console.warn('11!');
+  }
+}
+
+export default class Entity {
+  constructor() {
+    this.pos = new Vec2(0, 0);
+    this.vel = new Vec2(0, 0);
+
+    this.traits = [];
+  }
+
+  addTrait(trait) {
+    this.traits.push(trait);
+    this[trait.NAME] = trait;
+  }
+
+  update(deltatTime) {
+    this.traits.forEach(trait => {
+      trait.update(this, deltatTime);
+    });
+  }
+}
---
+++ public/js/layers.js
@@ -0,0 +1,29 @@
+function drawBackground(background, ctx, sprites) {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
+    for (let x = x1; x < x2; ++x) {
+      for (let y = y1; y < y2; ++y) {
+        sprites.drawTile(background.tile, ctx, x, y);
+      }
+    }
+  });
+}
+
+export function createBackgroundLayer(backgrounds, sprites) {
+  const buffer = document.createElement('canvas');
+  buffer.width = 256;
+  buffer.height = 240;
+
+  backgrounds.forEach(background => {
+    drawBackground(background, buffer.getContext('2d'), sprites);
+  });
+
+  return function drawBackfroundLayer(ctx) {
+    ctx.drawImage(buffer, 0, 0);
+  }
+}
+
+export function createSpriteLayer(entity) {
+  return function drawSpriteLayer(ctx) {
+    entity.draw(ctx);
+  }
+}
