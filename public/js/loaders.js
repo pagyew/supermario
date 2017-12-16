29ba0d5d5c4d35c861985a18dd165e1f4963c333
6ecuw <fickleprogger@gmail.com>
2017-12-16 19:08:42 +0300

    Animation complete

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,6 +1,7 @@
 import Level from './Level.js';
 import SpriteSheet from './SpriteSheet.js';
 import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+import {createAnim} from './anim.js';
 
 export function loadImage(url) {
   return new Promise(resolve => {
@@ -47,7 +48,7 @@ function createTiles(level, backgrounds) {
   });
 }
 
-function loadSpriteSheet(name) {
+export function loadSpriteSheet(name) {
   return loadJSON(`/sprites/${name}.json`)
   .then(sheetSpec => Promise.all([
     sheetSpec,
@@ -59,12 +60,27 @@ function loadSpriteSheet(name) {
       sheetSpec.tileW,
       sheetSpec.tileH);
 
-    sheetSpec.tiles.forEach(tileSpec => {
-      sprites.defineTile(
-        tileSpec.name,
-        tileSpec.index[0],
-        tileSpec.index[1]);
-    });
+    if (sheetSpec.tiles) {
+      sheetSpec.tiles.forEach(tileSpec => {
+        sprites.defineTile(
+          tileSpec.name,
+          tileSpec.index[0],
+          tileSpec.index[1]);
+      });
+    }
+
+    if (sheetSpec.frames) {
+      sheetSpec.frames.forEach(frameSpec => {
+        sprites.define(frameSpec.name, ...frameSpec.rect);
+      });
+    }
+
+    if (sheetSpec.animations) {
+      sheetSpec.animations.forEach(animSpec => {
+        const animation = createAnim(animSpec.frames, animSpec.frameLen);
+        sprites.defineAnim(animSpec.name, animation);
+      });
+    }
 
     return sprites;
   });
