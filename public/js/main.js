78320f94c748ad3208bd209bd0896f0ddd5c9134
6ecuw <fickleprogger@gmail.com>
2017-12-17 22:52:15 +0300

    Mario Mechanics complete

--- public/js/main.js
+++ public/js/main.js
@@ -19,21 +19,25 @@ Promise.all([
 
   mario.pos.set(64, 180);
   
-  level.comp.layers.push(
+  /*level.comp.layers.push(
     createCollisionLayer(level),
     createCameraLayer(camera)
-  );
+  );*/
   
   level.entities.add(mario);
 
   const input = setupKeyboard(mario);
   input.listenTo(window);
 
-  setupMouseControl(canvas, mario, camera);
+  // setupMouseControl(canvas, mario, camera);
 
   const timer = new Timer(1/60);
   timer.update = function update(deltaTime) {
     level.update(deltaTime);
+
+    if (mario.pos.x > 100) {
+      camera.pos.x = mario.pos.x - 100;
+    }
     
     level.comp.draw(context, camera);
   }
