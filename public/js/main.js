04a37f4b4bc10c41400888c49623b350cd7913ce
6ecuw <fickleprogger@gmail.com>
2018-01-21 21:18:15 +0300

    JSON Entity Spawning complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,44 @@
 import Camera from "./Camera.js";
 import Timer from "./Timer.js";
 import Entity from "./Entity.js";
-import {loadLevel} from "./loaders/level.js";
+import {createLevelLoader} from "./loaders/level.js";
 import {loadEntities} from "./entities.js";
 import {createCollisionLayer, createCameraLayer} from "./layers.js";
 import {setupKeyboard} from "./input.js";
 import {setupMouseControl} from "./debug.js";
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
-
-Promise.all([
-  loadEntities(),
-  loadLevel('1-1')
-])
-.then(([entity, level]) => {
+async function main(canvas) {
+  const context = canvas.getContext('2d');
+  
+  const entityFactory = await loadEntities();
+  const loadLevel = await createLevelLoader(entityFactory);
+  
+  const level = await loadLevel('1-1'); 
+  
   const camera = new Camera();
   
-  const mario = entity.mario();
+  const mario = entityFactory.mario();
   mario.pos.set(64, 64);
   level.entities.add(mario);
 
-  const goomba = entity.goomba();
-  goomba.pos.x = 220;
-  level.entities.add(goomba);
-  
-  const koopa = entity.koopa();
-  koopa.pos.x = 260;
-  level.entities.add(koopa);
-  
   level.comp.layers.push(createCollisionLayer(level));
-
+  
   const input = setupKeyboard(mario);
   input.listenTo(window);
-
+  
   const timer = new Timer(1/60);
   timer.update = function update(deltaTime) {
     level.update(deltaTime);
-
+    
     if (mario.pos.x > 100) {
       camera.pos.x = mario.pos.x - 100;
     }
     
     level.comp.draw(context, camera);
   }
-
+  
   timer.start();
-});
+}
+
+const canvas = document.getElementById('screen');
+main(canvas);
