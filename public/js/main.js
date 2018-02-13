096eaa54300b65e4297fc2b3089b66aa9d62280a
6ecuw <fickleprogger@gmail.com>
2018-02-13 15:33:51 +0300

    Dashboard & Fonts complete

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,12 @@ import Timer from "./Timer.js";
 import Entity from "./Entity.js";
 import {createLevelLoader} from "./loaders/level.js";
 import {loadEntities} from "./entities.js";
-import {createCollisionLayer, createCameraLayer} from "./layers.js";
+import {createCollisionLayer} from "./layers/collision.js";
 import {setupKeyboard} from "./input.js";
 import {setupMouseControl} from "./debug.js";
 import PlayerController from "./traits/PlayerController.js";
+import {loadFont} from "./loaders/font.js";
+import { createDashboardLayer } from "./layers/dashboard.js";
 
 function createPlayerEnv(entity) {
   const playerEnv = new Entity();
@@ -20,7 +22,11 @@ function createPlayerEnv(entity) {
 async function main(canvas) {
   const context = canvas.getContext('2d');
   
-  const entityFactory = await loadEntities();
+  const [entityFactory, font] = await Promise.all([
+    loadEntities(),
+    loadFont(),
+  ]);
+
   const loadLevel = await createLevelLoader(entityFactory);
   
   const level = await loadLevel('1-1'); 
@@ -32,7 +38,8 @@ async function main(canvas) {
   const playerEnv = createPlayerEnv(mario);
   level.entities.add(playerEnv);
 
-  level.comp.layers.push(createCollisionLayer(level));
+  // level.comp.layers.push(createCollisionLayer(level));
+  level.comp.layers.push(createDashboardLayer(font, playerEnv));
   
   const input = setupKeyboard(mario);
   input.listenTo(window);
